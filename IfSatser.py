18b7390d3fdd8793895adf1c578e49d3a2e83fc5
Simon Odding <simon.odding@learnet.se>
2024-10-10 12:57:53 +0200

    l

---
+++ IfSatser.py
@@ -0,0 +1,49 @@
+# print("Hello World")
+# print(30 + 40)
+# print("I ' am teacher")
+# print("hej" , str(5))
+# print("30" + "40")
+# print("30")
+# print(float("30.5")+ int("40"))
+
+# name = "kalle"
+# familjName = "ekman"
+# print(name + familjName)
+# print("han heter", name,"", familjName)
+# name = input("Vad heter du?")
+# print("välkommen" , name)
+
+# a = 30
+# b = 6 
+
+# if a <= b:
+#     print("a mindre eller lika med b")
+# elif a > b:
+#     print("a är större än b")
+# elif a == b:
+#     print("a är lika med b")
+
+# name = input("vad heter du")
+# if name == "simon":
+#     print("lärare")
+# else:
+#     print("elev")
+
+# age = int(input("hur gammal är du")) 
+# if age >= 18:
+#     print("du är myndig")
+# else:
+#     print("du är inte myndig") 
+
+# resultat = int(input("vad fick du på provet"))
+# if resultat >= 20:
+#     print("grattis! du klarade provet.")
+# else:
+#     print("bättre tur nästa gång.")
+
+gränsen = int (input("gränsen var:"))
+resultat = int (input("jag fick"))
+if resultat >= gränsen:
+    print("du är godkänd")
+else:
+    print("du blev godkänd")
---
+++ ListOvning.py
@@ -0,0 +1,26 @@
+# lista =[1, 2, 3, 4, 5, 6]
+# print(lista)
+
+# sista_bokstäver = ["w", "x", "y", "z"]
+# print(sista_bokstäver)
+
+# apa = [[1, 2, 3],[4, 5, 6]]
+# print(apa)
+
+# blandad_lista = [1, 'två', 3, 'fyra']
+# print(blandad_lista)
+
+# primtal = [2, 3, 5, 7, 11]
+
+
+# tomma_listor = [[], [], []]
+
+
+
+
+
+
+
+
+
+
